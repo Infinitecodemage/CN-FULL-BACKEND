815dfc9a644011a62c725522a9db7894cad27a38
Infinitecodemage <prdriteshraj@gmail.com>
2023-05-20 00:44:33 +0530

    serving htmlfile : efficiently. load the file at server startup. Instead at every req.

--- nodebhai/basichttpserver/hello.js
+++ nodebhai/basichttpserver/hello.js
@@ -1,36 +1,50 @@
+const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
-const http = require('http');
+
+//cannot use import statement out of module.
 // import http from 'http';// loading the http module 
 
 const host = 'localhost';
-const port = 8000; // endpoit/door to Ip address.
+const port = 8000; 
 
+//efficient serving html files.
+let mainFile;
 const requestlistener = function(req, res){
-    // -- The path.resolve() method ensures that the resulting file 
-    //    path is normalized and valid across different platforms.
-    const filepath = path.resolve(__dirname, '../main.html')
-
-    fs.readFile(filepath)
-    .then(contents => {
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
-        res.end(contents);
-    })
-    .catch(err => {
-        res.writeHead(500);
-        res.end(err);
-        return;
-    })
-
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(mainFile);
 }
+
+
 // create our server via http module's createServer()
 const server= http.createServer(requestlistener);
-server.listen(port, host, (err) =>{
-    if(err){
-        console.log('error');
-    }else{
-        console.log(`server is running on http://${host}:${port}`);
-    }
+const filepath = path.resolve(__dirname, '../main.html');
 
-})
+// Efficient way of serving html file.
+// -- we shift the file reading logic from the 
+//  requestListener() function to our server startup.
+fs.readFile(filepath)
+    .then(contents => {
+        mainFile = contents;
+        server.listen(port, host, ()=>{
+            console.log(`Server is running on http://${host}:${port}`);
+        });
+        
+    })
+    .catch(err => {
+        console.log(`Could not read main.html file: ${err}`);
+        process.exit(1);
+    });
+
+
+
+
+// server.listen(port, host, (err) =>{
+//     if(err){
+//         console.log('error');
+//     }else{
+//         console.log(`server is running on http://${host}:${port}`);
+//     }
+
+// })
