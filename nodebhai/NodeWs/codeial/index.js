86c497450bcf3530d5d2ab33d334e15181e98990
Infinitecodemage <prdriteshraj@gmail.com>
2023-05-26 00:50:38 +0530

    Add a controller & a route to root path / (homepage).

--- nodebhai/NodeWs/codeial/index.js
+++ nodebhai/NodeWs/codeial/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.use(req, res, next){
-    
-}
+app.use('/', require('./routes'));
+
+
+app.use((req, res, next) => {
+    console.log(`req.method: ${req.method} | :*:*: | req.url: ${req.url}`);
+    next();
+});
 
 // Callback functions are commonly used in asynchronous programming, 
 // where the execution of a function may not immediately produce a result.
--- nodebhai/NodeWs/codeial/routes/index.js
+++ nodebhai/NodeWs/codeial/routes/index.js
@@ -1,6 +1,20 @@
-const express= require('express');
-const router = express.Router();
+// -- Imports the express module-framework for 
+// web app in node.js
+const express = require('express');
 
-console.log('***--router loaded');
+// --> creates a router object using this method.
+// provides a way to define routes and middleware
+// for handling requests in a modular and organized manner.
+const router = express.Router(); 
+// imports the module homeC_controller modules.
+// It has home function.
+const homeController = require('../controllers/home_controller');
 
+// defines a route handler for the GET request method onthe root path'/'
+// it associate the 'home' function as handler of '/'(root path) route.
+router.get('/', homeController.home);
+
+console.log('Router Loaded: - ');
+
+//-- exports the router object from the module.
 module.exports = router;
