21eed7c9c81bb9326bfdc5f7187a23d82bd14aff
Infinitecodemage <prdriteshraj@gmail.com>
2023-05-25 22:29:21 +0530

    Codeial: major project, setting up file structure

---
+++ nodebhai/NodeWs/codeial/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(req, res, next){
+    
+}
+
+// Callback functions are commonly used in asynchronous programming, 
+// where the execution of a function may not immediately produce a result.
+
+// The 'err' parameter in a callback function is typically used to handle or propagate errors 
+// that occur during the execution of the asynchronous operation.
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);       // -> Interpolation or Template string.
+    }console.log(`Your server is running on port: ${port}`);
+})
